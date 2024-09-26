2b245480beea4cfead42dd8703b48809c5822946
Benedict Ryan Martinez <147293207+Rymarmar@users.noreply.github.com>
2024-09-26 11:53:39 -0400

    main.py
    Finished the main file for this project, learned how to create a CSV file and write in it using python. Also learned how to store data and currently working on data_entry.py to take user inputs into the tracker

---
+++ main.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import csv
+from datetime import datetime
+
+class CSV:
+    CSV_FILE = "finance_data.csv"
+    COLUMNS = ["date", "amount", "category", "description"]
+    
+    @classmethod
+    def initialize_csv(cls):
+        try:
+            pd.read_csv(cls.CSV_FILE)
+        except FileNotFoundError:
+            df = pd.DataFrame(columns = cls.COLUMNS)
+            df.to_csv(cls.CSV_FILE, index = False)
+    '''
+    For example:
+    date, amount, category, description
+    20-07-2024, 125, Income, Salary
+    '''
+
+    #Storing this in Python Dictionary
+    @classmethod
+    def add_entry(cls, date, amount, category, description):        
+        new_entry = {
+            "date": date,
+            "amount":amount,
+            "category": category,
+            "description": description
+        }
+        with open(cls.CSV_FILE, "a", newline = "") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames = cls.COLUMNS)
+            '''
+            CSV Writer: Takes a dictionary and write it into the csv file
+            '''
+            writer.writerow(new_entry)
+        
+        print("Entry added successfully")
+            
+        
+        
+
+
+CSV.initialize_csv()
+CSV.add_entry("26-09-2024", 125.65, "Income", "Salary")
